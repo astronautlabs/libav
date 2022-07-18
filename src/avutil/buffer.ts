5822f76a25eb9d893482d9e6b5d084e29458f709
William Lahti <wilahti@gmail.com>
2022-07-18 02:07:34 -0700

    remove extraneous import

--- src/avutil/buffer.ts
+++ src/avutil/buffer.ts
@@ -16,8 +16,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-import { AVBuffer as AVBufferImpl } from "../../binding";
-
 /**
  * AVBuffer is an API for reference-counted data buffers.
  *
